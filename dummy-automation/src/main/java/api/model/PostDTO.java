8cc338ff6b285f562d9712d03688ce3b1531c0fb
priscillasouza <p.souza7@gmail.com>
2023-10-25 10:47:11 -0300

    feat(dummy-api-automation): postdto added to project

---
+++ dummy-automation/src/main/java/api/model/PostDTO.java
@@ -0,0 +1,13 @@
+package api.model;
+
+public class PostDTO {
+
+    public String text;
+    public String image;
+    public int likes;
+    public String link;
+    public String[] tags;
+    public String publishDate;
+    public String owner;
+    public String id;
+}
